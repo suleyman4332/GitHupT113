e5ecd28b9e53380f38caf7a500530014aba27c36
suleyman4332 <suleymannunall433@gmail.com>
2023-03-27 22:43:42 +0300

    first

---
+++ src/gitHup_ilkDers.java
@@ -0,0 +1,11 @@
+public class gitHup_ilkDers {
+    public static void main(String[] args) {
+        System.out.println("hello GitUp");
+        System.out.println("----------------");
+        System.out.println("have a wise day ");
+
+
+    }
+
+
+}
